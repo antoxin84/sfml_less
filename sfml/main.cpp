a1968a7a2a8d5f2f0938a5c45c257c226d4eb075
Linnik Anton <linn84@mail.ru>
2018-01-27 17:14:51 +0200

    first commit

---
+++ sfml/main.cpp
@@ -0,0 +1,31 @@
+#include <SFML/Graphics.hpp>
+using namespace sf;
+int main()
+{
+	RenderWindow window(VideoMode(640, 480), "Lesson");
+	Image heroImage; // создание объекта Image (изображение)
+	heroImage.loadFromFile("img/hero.png"); //загружаем в него файл
+	Texture heroTexture; //создание объекта Texture(текстура)
+	heroTexture.loadFromImage(heroImage);// передаём в него объект heroImage
+	Sprite heroSprite; //создаём объект Sprite(спрайт)
+	heroSprite.setTexture(heroTexture);//передаём в него объект Texture (текстуры)
+	heroSprite.setPosition(50, 25);//задаём координаты появления спрайта
+	//проверка изминений git
+	//проверка 2
+
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(heroSprite);
+		window.display();
+	}
+
+	return 0;
+}
