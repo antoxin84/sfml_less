139b284d0d2ac7e656d46e9aaf03311143e7c1d5
Linnik Anton <linn84@mail.ru>
2018-01-27 20:53:34 +0200

    Moving

--- sfml/main.cpp
+++ sfml/main.cpp
@@ -11,6 +11,7 @@ int main()
 	heroSprite.setTexture(heroTexture);//передаЄм в него объект Texture (текстуры)
 	heroSprite.setPosition(50, 25);//задаЄм координаты по€влени€ спрайта
 	heroSprite.setTextureRect(IntRect(0,192,96,96)); // ѕолучили нужный пр€моугольник с котом
+	
 
 	///////////////////////////////////////////////////////////////////////////////////
 
@@ -23,6 +24,10 @@ int main()
 				window.close();
 		}
 
+		if (Keyboard::isKeyPressed(Keyboard::Left)||(Keyboard::isKeyPressed(Keyboard::A))) { heroSprite.move(-0.1, 0); heroSprite.setTextureRect(IntRect(0, 96, 96, 96)); }//координата Y, на которой герой изображен идущим влево равна 96
+		if (Keyboard::isKeyPressed(Keyboard::Right) || (Keyboard::isKeyPressed(Keyboard::D))){ heroSprite.move(0.1, 0); heroSprite.setTextureRect(IntRect(0, 192, 96, 96)); } //координата Y, на которой герой изображен идущем вправо равна 96+96=192
+		if (Keyboard::isKeyPressed(Keyboard::Up) || (Keyboard::isKeyPressed(Keyboard::W))) { heroSprite.move(0,-0.1); heroSprite.setTextureRect(IntRect(0, 288, 96, 96)); }//координата Y на которой герой изображен идущим вверх равна 288
+		if (Keyboard::isKeyPressed(Keyboard::Down) || (Keyboard::isKeyPressed(Keyboard::S))) { heroSprite.move(0,0.1); heroSprite.setTextureRect(IntRect(0, 0, 96, 96)); } //координата 0, это верхн€€ часть изображени€ с героем, от нее и отталкиваемс€ ровными квадратиками по 96. 
 		window.clear();
 		window.draw(heroSprite);
 		window.display();
