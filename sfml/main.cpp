606b761b2de479bc0c1c36dcab6a4dc4e3fd4b61
Linnik Anton <linn84@mail.ru>
2018-01-28 16:33:53 +0200

    time

--- sfml/main.cpp
+++ sfml/main.cpp
@@ -1,7 +1,9 @@
 #include <SFML/Graphics.hpp>
+//#include <iostream> 
 using namespace sf;
 int main()
 {
+	Clock clock;//создаем переменную времени, т.о. привязка ко времени(а не загруженности/мощности процессора)
 	RenderWindow window(VideoMode(640, 480), "Lesson");
 	Image heroImage; // создание объекта Image (изображение)
 	heroImage.loadFromFile("img/hero.png"); //загружаем в него файл
@@ -17,6 +19,10 @@ int main()
 
 	while (window.isOpen())
 	{
+		float time = clock.getElapsedTime().asMicroseconds();// дать прошедее времяя в микросекундах
+		clock.restart();//перегружаем время
+		time = time / 800; //скорость игры
+		
 		Event event;
 		while (window.pollEvent(event))
 		{
@@ -24,10 +30,10 @@ int main()
 				window.close();
 		}
 
-		if (Keyboard::isKeyPressed(Keyboard::Left)||(Keyboard::isKeyPressed(Keyboard::A))) { heroSprite.move(-0.1, 0); heroSprite.setTextureRect(IntRect(0, 96, 96, 96)); }//координата Y, на которой герой изображен идущим влево равна 96
-		if (Keyboard::isKeyPressed(Keyboard::Right) || (Keyboard::isKeyPressed(Keyboard::D))){ heroSprite.move(0.1, 0); heroSprite.setTextureRect(IntRect(0, 192, 96, 96)); } //координата Y, на которой герой изображен идущем вправо равна 96+96=192
-		if (Keyboard::isKeyPressed(Keyboard::Up) || (Keyboard::isKeyPressed(Keyboard::W))) { heroSprite.move(0,-0.1); heroSprite.setTextureRect(IntRect(0, 288, 96, 96)); }//координата Y на которой герой изображен идущим вверх равна 288
-		if (Keyboard::isKeyPressed(Keyboard::Down) || (Keyboard::isKeyPressed(Keyboard::S))) { heroSprite.move(0,0.1); heroSprite.setTextureRect(IntRect(0, 0, 96, 96)); } //координата 0, это верхняя часть изображения с героем, от нее и отталкиваемся ровными квадратиками по 96. 
+		if (Keyboard::isKeyPressed(Keyboard::Left)||(Keyboard::isKeyPressed(Keyboard::A))) { heroSprite.move(-0.1*time, 0); heroSprite.setTextureRect(IntRect(0, 96, 96, 96)); }//координата Y, на которой герой изображен идущим влево равна 96
+		if (Keyboard::isKeyPressed(Keyboard::Right) || (Keyboard::isKeyPressed(Keyboard::D))){ heroSprite.move(0.1*time, 0); heroSprite.setTextureRect(IntRect(0, 192, 96, 96)); } //координата Y, на которой герой изображен идущем вправо равна 96+96=192
+		if (Keyboard::isKeyPressed(Keyboard::Up) || (Keyboard::isKeyPressed(Keyboard::W))) { heroSprite.move(0,-0.1*time); heroSprite.setTextureRect(IntRect(0, 288, 96, 96)); }//координата Y на которой герой изображен идущим вверх равна 288
+		if (Keyboard::isKeyPressed(Keyboard::Down) || (Keyboard::isKeyPressed(Keyboard::S))) { heroSprite.move(0,0.1*time); heroSprite.setTextureRect(IntRect(0, 0, 96, 96)); } //координата 0, это верхняя часть изображения с героем, от нее и отталкиваемся ровными квадратиками по 96. 
 		window.clear();
 		window.draw(heroSprite);
 		window.display();
